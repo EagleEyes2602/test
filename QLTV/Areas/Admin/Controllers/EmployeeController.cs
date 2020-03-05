6547b8e54b7c35b303f05d719abf69f86c1a92ba
EagleEyes <qquocviet2602@gmail.com>
2020-03-05 18:46:59 +0700

    abc

---
+++ QLTV/Areas/Admin/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using QLTV.Models.Entity;
+using QLTV.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLTV.Areas.Admin.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public QLTVEntities db = new QLTVEntities();
+        // GET: Admin/Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Pagging(int pageIndex = 1, int pageSize = 1)
+        {
+            EmployeeViewModel model = new EmployeeViewModel();
+            int upper = (pageIndex - 1) * pageSize;
+            var emps = db.Employees.OrderBy(x => x.Id);
+            model.Employees = emps.Skip(upper).Take(pageSize).ToList();
+            model.PageIndex = pageIndex;
+            model.PageSize = pageSize;
+            model.TotalRecord = emps.Count();
+            decimal totalPage = (decimal)model.TotalRecord / model.PageSize;
+            model.TotalPage = decimal.ToInt32(Math.Ceiling(totalPage));
+            return PartialView(model);
+        }
+    }
+}
---
+++ QLTV/Models/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using QLTV.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLTV.Models.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public List<Employee> Employees { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+        public int TotalPage { get; set; }
+        public int TotalRecord { get; set; }
+    }
+}
